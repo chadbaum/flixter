0d9303d86c7fae24c346a84ff4888b17d3165168
Chad Baum <chadbaum@gmail.com>
2016-06-07 16:27:56 +0000

    Added authorization for being enrolled in courses

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
 
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_user.enrolled_in?(current_lesson.section.course)
+    else
+        redirect_to course_path(current_lesson), alert: 'You are not enrolled in this course!'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
