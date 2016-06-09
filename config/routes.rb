a51a33b89f10982650cf721d1092616b56fe35f2
Chad Baum <chadbaum@gmail.com>
2016-06-09 01:41:40 +0000

    Added drag and drop to lessons list with jQueryUI, a new lessons#update API and an Ajax put request

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
-    resource :enrollments, only: :create
+    resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :lessons, only: [:update] 
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
