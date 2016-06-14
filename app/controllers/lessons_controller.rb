eb574f366a8a38b9cd83e82cb14787ec8f85399f
Chad Baum <chadbaum@gmail.com>
2016-06-14 13:35:19 +0000

    Bug fix to lessons controller and refactored authorization method

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,9 +8,8 @@ class LessonsController < ApplicationController
   private
 
   def require_authorized_for_current_course
-    if current_user.enrolled_in?(current_lesson.section.course)
-    else
-        redirect_to course_path(current_lesson), alert: 'You are not enrolled in this course!'
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+        redirect_to course_path(current_lesson.section.course), alert: 'You are not enrolled in this course!'
     end
   end
 
